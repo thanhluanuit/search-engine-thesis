9c19a6e38fea82a3c4bc3b27907ac9a0e8f2cb8f
thanhluanuit <thanhluan.uit@gmail.com>
2016-08-27 08:53:54 +0700

    Change logic search to multi matching for description and annotation fields

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -10,19 +10,31 @@ module Searchable
     settings index: { number_of_shards: 1 } do
       mappings dynamic: 'false' do
         indexes :description, analyzer: 'english'
+
+        indexes :annotations, type: 'nested' do
+          indexes :name, analyzer: 'snowball'
+        end
       end
     end
   end
 
   def as_indexed_json(options={})
-    as_json( only: [:description] )
+    as_json(
+      only: [:description],
+      include: {
+        annotations: { only: :name }
+      }
+    )
   end
 
   def self.search(query) 
     __elasticsearch__.search( 
-      { 
-        query: { 
-          match:  { description: query }
+      {
+        query: {
+          multi_match: {
+            query: query,
+            fields: ["description", "annotations.name"]
+          }
         }
       }
     )
